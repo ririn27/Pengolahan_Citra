530000e6ade5c27ad3b7229509ea22bae8c059ed
Ririn Nofrima <115934294+ririn27@users.noreply.github.com>
2024-05-20 22:04:13 +0700

    puh

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : Ririn Nofrima')
+st.subheader('NIM : 312210025')
+st.subheader('Kelas : TI.22.B1')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/jpg.jpg", caption="RIRIN", use_column_width="Always", width=200)
+
+
+st.link_button("Instagram", "https://www.instagram.com/ri2nnfrma/")
+st.link_button("Github", "https://github.com/ririn27")
